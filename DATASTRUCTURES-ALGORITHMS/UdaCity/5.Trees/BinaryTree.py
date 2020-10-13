783f5629d1b8871e8ceaa56402eb6f23b19efcd7
Durgasuresh Kagitha <durgasuresh.kagitha@oracle.com>
2020-10-14 04:34:54 +0530

    adding binary tree and BST

--- DATASTRUCTURES-ALGORITHMS/UdaCity/5.Trees/BinaryTree.py
+++ DATASTRUCTURES-ALGORITHMS/UdaCity/5.Trees/BinaryTree.py
@@ -13,23 +13,32 @@ class BinaryTree(object):
         """Return True if the value
         is in the tree, return
         False otherwise."""
-
-        return False
+        return self.preorder_search(self.root, find_val)
 
     def print_tree(self):
         """Print out all tree nodes
         as they are visited in
         a pre-order traversal."""
-        return ""
+        return self.preorder_print(self.root, "")[:-1]
 
     def preorder_search(self, start, find_val):
         """Helper method - use this to create a
         recursive search solution."""
+        if start:
+            if start.value == find_val:
+                return True
+            else:
+                return self.preorder_search(start.left, find_val) or self.preorder_search(start.right, find_val)
+
         return False
 
     def preorder_print(self, start, traversal):
         """Helper method - use this to create a
         recursive print solution."""
+        if start:
+            traversal += str(start.value) + "-"
+            traversal = self.preorder_print(start.left, traversal)
+            traversal = self.preorder_print(start.right, traversal)
         return traversal
 
 
